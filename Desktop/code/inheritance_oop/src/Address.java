d947d136b70396ca90f571138b9ac9c519e28672
Qedir <qedirbabayev3@gmail.com>
2024-06-13 15:37:46 +0400

    f

---
+++ Desktop/code/inheritance_oop/src/Address.java
@@ -0,0 +1,29 @@
+public class Address {
+    private int id;
+    private  String countryName;
+    private String cityName;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getCountryName() {
+        return countryName;
+    }
+
+    public void setCountryName(String countryName) {
+        this.countryName = countryName;
+    }
+
+    public String getCityName() {
+        return cityName;
+    }
+
+    public void setCityName(String cityName) {
+        this.cityName = cityName;
+    }
+}
---
+++ Desktop/code/inheritance_oop/src/Person.java
@@ -0,0 +1,39 @@
+public class Person {
+    private int finCode;
+    private String name;
+    private String surname;
+    private String address;
+
+
+    public int getFinCode() {
+        return finCode;
+    }
+
+    public void setFinCode(int finCode) {
+        this.finCode = finCode;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+}
